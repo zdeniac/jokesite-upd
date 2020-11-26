06e370f6753166367c7d10b7e6f9e1028038996f
Zdenyák József <zdenyakj@gmail.com>
2020-11-26 16:41:05 +0100

    Beküldő nevének és e-mail címének megjelenítése.

--- jokes.php
+++ jokes.php
@@ -5,7 +5,7 @@
 		$pdo = new PDO('mysql:host=localhost;port=3308;dbname=jokes_crud;charset=utf8', 'root', '');
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-		$sql = 'SELECT `id`, `text` FROM `joke`';
+		$sql = "SELECT `joke`.`id`, `text`, `name`, `email` FROM `joke` INNER JOIN `author` ON `authorid` = `author`.`id`";
 		$jokes = $pdo->query($sql);
 
 		$pdo = null;
--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -2,6 +2,11 @@
 <blockquote>
 	<p>
 		<?=htmlspecialchars($joke['text'], ENT_QUOTES, 'UTF-8')?>
+		<span>
+			<a href="mailto:<?=htmlspecialchars($joke['email'], ENT_QUOTES, 'UTF-8')?>">
+			<?=htmlspecialchars($joke['name'], ENT_QUOTES, 'UTF-8')?>
+			</a>
+		</span>
 		<form action="deletejoke.php" method="post">
 			<input type="hidden" name="id" value="<?=$joke['id']?>">
 			<input type="submit" value="Törlés">
